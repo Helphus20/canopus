6d5c4e78bc1acfa15b44a57afd179c0515b3ce39
Felipe da Silva <felipedasilvabispodossantos@gmail.com>
2024-10-10 23:20:44 -0300

    :sparkles: feat: Adicionando páginas de cadastro e login

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const path = require('path')
 const { getAllUsers } = require('../Controllers/UserController');
 
 const router = express.Router();
 
 // Routes
+router.get('/', (req, res) => {
+    res.render('index'); // o método render é usado para processar templates dinâmicos, o sendfile não se aplicaria pq ele serve arquivos estáticos
+
+});
+
+router.get('/cadastro', (req, res) => {
+    res.render('cadastro');
+})
+
 router.get('/users', getAllUsers);
 
 module.exports = router;
--- script.js
+++ script.js
@@ -8,6 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 //módulo de rotas
 app.use(userRoutes);
 
+// aqui eu estou dizendo ao express que estou usando o ejs como motor de visualização (engine view)
+app.set('view engine', 'ejs');// para posteriormente transformar os arquivos ejs em um html dinâmico
+app.set('views', './Views'); // defino em qual pasta o express deve procurar pelas views (templates ejs)
+
+app.use(express.static('public')); // Para servir arquivos estáticos como CSS, imagens, etc.
+
+
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
